65a7692c38bc5fc71235f19966baa411898cef8c
shijiale0609 <shijiale1995@gmail.com>
2023-09-09 15:24:25 -0400

    plot.py update

--- utils/plot.py
+++ utils/plot.py
@@ -3,18 +3,33 @@ import networkx as nx
 
 def graph(tmp_graph):
     """Plots networkx graph."""
+
     node_list = [
         tmp_graph.nodes[idx]['label'] 
             for idx in range(len(tmp_graph.nodes))]
-    nx.draw_networkx(tmp_graph)
-    node_dict = dict(zip(range(len(node_list)), node_list))   
-    print(node_dict)
-    pos = nx.spring_layout(tmp_graph)
+
+    node_dict = dict(zip(range(len(node_list)), node_list))
+
+    node_set = sorted(list(set(node_list)))
+
+    pos = nx.spring_layout(tmp_graph)  
+
     plt.figure(figsize=(8, 8))
     plt.figure()
     plt.axis('off')
 
     color_list = []
+    color_dict = {
+        "R0":'#0070C0',
+        "R1":'#C00000',
+        "E1":'#FFD966',
+        "E2":'#A9D18E',
+        "R2":'#00B0F0',
+        "R3":'#D31DC2',
+        "unknown":'#D9D9D9',  
+    }
+
+
     for i in range(0, len(node_list)):
         if node_list[i] == "R0":
             color_list.append('#0070C0')
@@ -28,12 +43,21 @@ def graph(tmp_graph):
             color_list.append('#00B0F0')
         elif node_list[i] == "R3":
             color_list.append('#D31DC2')
+        else:
+            color_list.append('#D9D9D9')
+          
+    nx.draw_networkx(tmp_graph, pos, alpha = 1, node_size=300, node_color='white')
+    nx.draw_networkx_nodes(tmp_graph, pos, node_size=400, node_color=color_list, alpha = 1)
+    nx.draw_networkx_edges(tmp_graph, pos, alpha=1,width=4.0)
 
-    nx.draw_networkx(g, pos, alpha = 1, node_size=300, node_color='white')
-    nx.draw_networkx_nodes(g, pos, node_size=400, node_color=color_list, alpha = 1)
-    nx.draw_networkx_edges(g, pos, alpha=1,width=4.0)
-
+    for i in range(0,len(node_set)):
+        plt.gca().scatter([],[], color=color_dict[node_set[i]] , label=node_set[i])
+    
+    plt.legend(loc='best',fontsize = 10)
     plt.show()
+
+    print(node_dict)
+
 
 def similarity_matrix(matrix):
     """Plots similarity matrix."""
