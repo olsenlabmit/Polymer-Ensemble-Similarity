90d29993fc28ad834d22d49a26f1907667918fe7
olsenlabmit <49217628+olsenlabmit@users.noreply.github.com>
2023-09-09 14:58:50 -0400

    Add files via upload

--- utils/plot.py
+++ utils/plot.py
@@ -7,7 +7,33 @@ def graph(tmp_graph):
         tmp_graph.nodes[idx]['label'] 
             for idx in range(len(tmp_graph.nodes))]
     nx.draw_networkx(tmp_graph)
-    print(dict(zip(range(len(node_list)), node_list)))
+    node_dict = dict(zip(range(len(node_list)), node_list))   
+    print(node_dict)
+    pos = nx.spring_layout(tmp_graph)
+    plt.figure(figsize=(8, 8))
+    plt.figure()
+    plt.axis('off')
+
+    color_list = []
+    for i in range(0, len(node_list)):
+        if node_list[i] == "R0":
+            color_list.append('#0070C0')
+        elif node_list[i] == "R1":
+            color_list.append('#C00000')
+        elif node_list[i] == "E1":
+            color_list.append('#FFD966')
+        elif node_list[i] == "E2":
+            color_list.append('#A9D18E')
+        elif node_list[i] == "R2":
+            color_list.append('#00B0F0')
+        elif node_list[i] == "R3":
+            color_list.append('#D31DC2')
+
+    nx.draw_networkx(g, pos, alpha = 1, node_size=300, node_color='white')
+    nx.draw_networkx_nodes(g, pos, node_size=400, node_color=color_list, alpha = 1)
+    nx.draw_networkx_edges(g, pos, alpha=1,width=4.0)
+
+    plt.show()
 
 def similarity_matrix(matrix):
     """Plots similarity matrix."""
